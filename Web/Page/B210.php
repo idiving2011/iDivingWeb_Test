be0ae4ec6ba2bd1d3692cb0b07c70dcb74c3250b
yuanjht <ginhong714@yahoo.com.tw>
2014-03-10 23:32:04 +0800

    更新 iDiving 介紹 資料

--- Web/Page/B210.php
+++ Web/Page/B210.php
@@ -16,6 +16,9 @@
 	<div id="section" sight>
 		<div id="aim" class="ArticleBox"></div>
 		<div id="advantage" class="ArticleBox"></div>
+		<div id="open" class="ArticleBox"></div>
+		<div id="contact" class="ArticleBox"></div>
+		<div id="remittance" class="ArticleBox"></div>
 	</div>
 	<?php include "../Part/Foot.php" ; ?>
 </body>
