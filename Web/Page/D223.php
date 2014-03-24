5fbdd2504ba2bdf81f81e394ceef5d29240a8ec4
yuanjht <ginhong714@yahoo.com.tw>
2014-03-25 01:19:33 +0800

    新增明多洛資料

---
+++ Web/Page/D223.php
@@ -0,0 +1,21 @@
+﻿<?php include "../Common.php" ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+    <meta name="Extra" content="" />
+    <meta name="BasePath" content="../" />
+    <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
+    <script type="text/javascript" src="../Script/jquery.js"></script>
+    <script type="text/javascript" src="../Script/Loader.js"></script>
+</head>
+<body>
+    <?php include "../Part/Head.php"; ?>
+    <?php include "../Part/Cap.php"; ?>
+    <div id="section" sight>
+		<div id="Anilao" class="ArticleBox"></div>
+    </div>
+    <?php include "../Part/Foot.php" ; ?>
+</body>
+</html>
