6bcd2720ca54f0ee8ae7bd3abc20e172185671c0
yuanjht <ginhong714@yahoo.com.tw>
2014-07-02 00:05:21 +0800

    刪除OWD以外的學費

--- Web/Page/C123.php
+++ Web/Page/C123.php
@@ -16,9 +16,6 @@
     <?php include "../Part/Cap.php"; ?>
     <div id="section" sight>
 		<div id="OWD" class="ArticleBox"></div>
-		<div id="AOWD" class="ArticleBox"></div>
-		<div id="NITROX" class="ArticleBox"></div>
-		<div id="SR" class="ArticleBox"></div>
     </div>
     <?php include "../Part/Foot.php" ; ?>
 </body>
