588b20e30d1250377750ead3ead30fa39e9732fd
yuanjht <ginhong714@yahoo.com.tw>
2015-03-16 00:54:21 +0800

    更新專長潛水員內容

--- Web/Page/C310.php
+++ Web/Page/C310.php
@@ -19,47 +19,70 @@
 		<div id="specialty" class="Block"></div>
 
 		<!-- 深潛 -->
-		<div id="notice1" class="ArticleBox"></div>
+		<div id="curriculum1" class="ArticleBox"></div>
 		<div id="plan1" class="ArticleBox"></div>
 		<div id="tuition1" class="ArticleBox"></div>
 
-		<!-- 高氧 -->
-		<div id="notice2" class="ArticleBox"></div>
+		<!-- 夜潛 -->
+		<div id="curriculum2" class="ArticleBox"></div>
 		<div id="plan2" class="ArticleBox"></div>
 		<div id="tuition2" class="ArticleBox"></div>
 
-		<!-- 夜潛 -->
-		<div id="notice3" class="ArticleBox"></div>
+		<!-- 高氧 -->
+		<div id="curriculum3" class="ArticleBox"></div>
 		<div id="plan3" class="ArticleBox"></div>
 		<div id="tuition3" class="ArticleBox"></div>
 
 		<!-- 導航 -->
-		<div id="notice4" class="ArticleBox"></div>
+		<div id="curriculum4" class="ArticleBox"></div>
 		<div id="plan4" class="ArticleBox"></div>
 		<div id="tuition4" class="ArticleBox"></div>
 
-		<!-- 船潛 -->
-		<div id="notice5" class="ArticleBox"></div>
+		<!-- 搜索與找回 -->
+		<div id="curriculum5" class="ArticleBox"></div>
 		<div id="plan5" class="ArticleBox"></div>
 		<div id="tuition5" class="ArticleBox"></div>
 
-		<!-- 電腦錶 -->
-		<div id="notice6" class="ArticleBox"></div>
+		<!-- 壓力與救援 -->
+		<div id="curriculum6" class="ArticleBox"></div>
 		<div id="plan6" class="ArticleBox"></div>
 		<div id="tuition6" class="ArticleBox"></div>
 
 		<!-- 完美中性浮力 -->
-		<div id="notice7" class="ArticleBox"></div>
+		<div id="curriculum7" class="ArticleBox"></div>
 		<div id="plan7" class="ArticleBox"></div>
 		<div id="tuition7" class="ArticleBox"></div>
 
-		<!-- 壓力與救援 -->
-		<div id="notice8" class="ArticleBox"></div>
+		<!-- 船潛 -->
+		<div id="curriculum8" class="ArticleBox"></div>
 		<div id="plan8" class="ArticleBox"></div>
 		<div id="tuition8" class="ArticleBox"></div>
 
+		<!-- 電腦錶 -->
+		<div id="curriculum9" class="ArticleBox"></div>
+		<div id="plan9" class="ArticleBox"></div>
+		<div id="tuition9" class="ArticleBox"></div>
+
 		<div id="register" class="ArticleBox"></div>
 		<div id="processing" class="ArticleBox"></div>
+		<div id="notice" class="ArticleBox"></div>
+
+		<!-- 深潛 -->
+		<!--<div id="notice1" class="ArticleBox"></div>-->
+		<!-- 夜潛 -->
+		<!--<div id="notice2" class="ArticleBox"></div>-->
+		<!-- 高氧 -->
+		<!--<div id="notice3" class="ArticleBox"></div>-->
+		<!-- 導航 -->
+		<!--<div id="notice4" class="ArticleBox"></div>-->
+		<!-- 船潛 -->
+		<!--<div id="notice5" class="ArticleBox"></div>-->
+		<!-- 電腦錶 -->
+		<!--<div id="notice6" class="ArticleBox"></div>-->
+		<!-- 完美中性浮力 -->
+		<!--<div id="notice7" class="ArticleBox"></div>-->
+		<!-- 壓力與救援 -->
+		<!--<div id="notice8" class="ArticleBox"></div>-->
 	</div>
 	<?php include "../Part/Foot.php" ; ?>
 </body>
