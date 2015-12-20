94c9f6c53d31c78ab8b25c5aa1c57f1d3431f3ea
yuanjht <ginhong714@yahoo.com.tw>
2015-12-20 22:29:19 +0800

    2016 自由潛水

--- Web/Page/CFD110.php
+++ Web/Page/CFD110.php
@@ -16,6 +16,12 @@
     <?php include "../Part/Cap.php"; ?>
     <div id="section" sight>
 		<div id="intro" class="ArticleBox"></div>
+		<div id="curriculum" class="ArticleBox"></div>
+		<div id="plan" class="ArticleBox"></div>
+		<div id="tuition" class="ArticleBox"></div>
+		<div id="register" class="ArticleBox"></div>
+		<div id="processing" class="ArticleBox"></div>
+		<div id="notice" class="ArticleBox"></div>
     </div>
     <?php include "../Part/Foot.php" ; ?>
 </body>
