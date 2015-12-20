d1efb2aba469b015683880c1897e9402a2a953aa
yuanjht <ginhong714@yahoo.com.tw>
2015-12-20 22:39:07 +0800

    2016 高階教練

--- Web/Page/C430.php
+++ Web/Page/C430.php
@@ -6,7 +6,7 @@
     <title></title>
     <meta name="Extra" content="" />
     <meta name="BasePath" content="../" />
-    <meta name="Data" content = "<?php echo getDataFileName(); ?>"/>
+    <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
 	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
     <script type="text/javascript" src="../Script/jquery.js"></script>
     <script type="text/javascript" src="../Script/Loader.js"></script>
@@ -15,6 +15,17 @@
     <?php include "../Part/Head.php"; ?>
     <?php include "../Part/Cap.php"; ?>
     <div id="section" sight>
+	  <div id="intro" class="ArticleBox"></div>
+	  <div id="specialty" class="Block"></div>
+
+	  <!-- 潛水主管教練(DCSI) -->
+	  <div id="intro1" class="ArticleBox"></div>
+	  <!-- 教練長(MI) -->
+	  <div id="intro2" class="ArticleBox"></div>
+	  <!-- 教練訓練官(IT) -->
+	  <div id="intro3" class="ArticleBox"></div>
+
+
     </div>
     <?php include "../Part/Foot.php" ; ?>
 </body>
