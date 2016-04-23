67aeb33cc2a3ee86a85de3d62c0dd84662182e1a
YUAN <YUAN@DESKTOP-YUAN>
2016-04-23 20:35:01 +0800

    修改營業時間

--- Web/Part/Foot.php
+++ Web/Part/Foot.php
@@ -1,5 +1,5 @@
 <div id="footer" sight>
-    <span class="CopyRight">營業時間：周一公休，周二-六15:00 - 22:00 (冬季晚上營業至21:00)，周日15:00 - 18:00</span><br/>
+    <span class="CopyRight">營業時間：周一公休，周二-六：4-10月 14:00 - 22:00 / 11-3月 15:00-21:00，周日：13:00 - 19:00</span><br/>
     <span class="CopyRight">愛潛水股份有限公司版權所有</span>
     <span class="CopyRight">© 2013 iDiving Co., Ltd.</span><br style="display:block" />
     <span class="Address">電話：<a href="tel:0228827696">0228827696</a></span>
