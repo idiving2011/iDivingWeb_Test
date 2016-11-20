d70cfd6ee714b5adc66d81059ae12323e84aee4e
Maxwell <ajkmaxwell@gmail.com>
2016-11-20 23:11:41 +0800

    修改資料行為繫結時間

--- Web/Script/Extra/C120.php.js
+++ Web/Script/Extra/C120.php.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../jquery.js" />
 
 /* === Override Loader.js === */
-function extension() {
+function afterData() {
     $(".Link[data-tab]").click(function () {
         $(".Tab .Tag:nth-of-type(" + $(this).attr("data-tab") + ")").click();
     });
