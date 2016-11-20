3aae604da98bb6017be8c7818f77a1b0e076348c
Maxwell <ajkmaxwell@gmail.com>
2016-11-20 22:28:59 +0800

    增加操作 Tab 的行為

---
+++ Web/Script/Extra/C120.php.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../jquery.js" />
+
+/* === Override Loader.js === */
+function extension() {
+    $(".Link[data-tab]").click(function () {
+        $(".Tab .Tag:nth-of-type(" + $(this).attr("data-tab") + ")").click();
+    });
+}
