4487eed066b86c337bb337faadc042682a018f0b
YUAN <YUAN@DESKTOP-YUAN>
2017-04-02 22:48:08 +0800

    更新重裝資訊。

--- Web/Page/B320.php
+++ Web/Page/B320.php
@@ -3,18 +3,18 @@
 
 <html>
 <head>
-    <title></title>
-    <meta name="Extra" content="B320.php.less" />
-    <meta name="BasePath" content="../" />
-    <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
+  <title></title>
+  <meta name="Extra" content="B320.php.less" />
+  <meta name="BasePath" content="../" />
+  <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
 	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
-    <script type="text/javascript" src="../Script/jquery.js"></script>
-    <script type="text/javascript" src="../Script/Loader.js"></script>
+  <script type="text/javascript" src="../Script/jquery.js"></script>
+  <script type="text/javascript" src="../Script/Loader.js"></script>
 </head>
 <body>
-    <?php include "../Part/Head.php"; ?>
-    <?php include "../Part/Cap.php"; ?>
-    <div id="section" sight>
+  <?php include "../Part/Head.php"; ?>
+  <?php include "../Part/Cap.php"; ?>
+  <div id="section" sight>
 		<div id="intro" class="ArticleBox"></div>
 		<div id="BC" class="ArticleBox"></div>
 		<div id="Regulators" class="ArticleBox"></div>
@@ -22,7 +22,8 @@
 		<div id="Instruments" class="ArticleBox"></div>
 		<div id="Computer" class="ArticleBox"></div>
 		<div id="Sets" class="ArticleBox"></div>
-    </div>
-    <?php include "../Part/Foot.php" ; ?>
+    <div id="camera" class="ArticleBox"></div>
+  </div>
+  <?php include "../Part/Foot.php" ; ?>
 </body>
 </html>
