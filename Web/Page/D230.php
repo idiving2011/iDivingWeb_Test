78d484108eefba8c4911aeb52e1dccd817c756db
YUAN <YUAN@DESKTOP-YUAN>
2017-10-02 22:42:36 +0800

    更新杜馬蓋地內容

--- Web/Page/D230.php
+++ Web/Page/D230.php
@@ -16,8 +16,9 @@
   <?php include "../Part/Cap.php"; ?>
   <div id="section" sight="">
     <div id="intro" class="ArticleBox"></div>
+    <div id="DiveSite" class="ArticleBox"></div>
     <div id="schedule" class="ArticleBox"></div>
-    <!--<div id="price" class="ArticleBox"></div>-->
+    <div id="price" class="ArticleBox"></div>
     <div id="notice" class="ArticleBox"></div>
     <div id="agency" class="ArticleBox"></div>
   </div>
