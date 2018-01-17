69173d38e80fbd942c34162e5fadab6f3a54931b
YUAN <YUAN@DESKTOP-YUAN>
2018-01-17 22:36:49 +0800

    建議行程新增為四天行程與五天行程

--- Web/Page/D210.php
+++ Web/Page/D210.php
@@ -4,21 +4,35 @@
 <html>
 <head>
   <title></title>
+  <meta name="Extra" content="C120.php.js" />
   <meta name="Extra" content="" />
   <meta name="BasePath" content="../" />
   <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
-	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
+  <link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
   <script type="text/javascript" src="../Script/jquery.js"></script>
   <script type="text/javascript" src="../Script/Loader.js"></script>
+  <Script>
+	
+  </script>
 </head>
 <body>
   <?php include "../Part/Head.php"; ?>
   <?php include "../Part/Cap.php"; ?>
   <div id="section" sight="">
+	<div id="dock" class="Block"></div>
     <div id="intro" class="ArticleBox"></div>
     <div id="DiveSite" class="ArticleBox"></div>
-    <div id="schedule" class="ArticleBox"></div>
-    <div id="price" class="ArticleBox"></div>
+
+	<div id="schedule" class="ArticleBox"></div>
+	<div id="ANILAO" class="Block"></div>
+	<!-- 四天行程 -->
+    <div id="schedule1" class="ArticleBox"></div>
+    <div id="price1" class="ArticleBox"></div>
+	<!-- 五天行程 -->
+	<div id="schedule2" class="ArticleBox"></div>
+    <div id="price2" class="ArticleBox"></div>
+
+	<!-- 共用 -->
     <div id="notice" class="ArticleBox"></div>
     <div id="agency" class="ArticleBox"></div>
   </div>
