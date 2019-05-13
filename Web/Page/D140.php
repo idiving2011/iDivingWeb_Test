a39014a2150b2b86f333ee39932d32852b9c8e1b
YUAN <YUAN@DESKTOP-YUAN>
2019-05-13 23:34:57 +0800

    更新綠島建議行程：水肺3天行程、自潛4天行程

--- Web/Page/D140.php
+++ Web/Page/D140.php
@@ -4,10 +4,11 @@
 <html>
 <head>
   <title></title>
+  <meta name="Extra" content="C120.php.js" />
   <meta name="Extra" content="" />
   <meta name="BasePath" content="../" />
   <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
-	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
+  <link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
   <script type="text/javascript" src="../Script/jquery.js"></script>
   <script type="text/javascript" src="../Script/Loader.js"></script>
 </head>
@@ -17,7 +18,17 @@
   <div id="section" sight="">
     <div id="intro" class="ArticleBox"></div>
     <div id="DiveSite" class="ArticleBox"></div>
+
     <div id="schedule" class="ArticleBox"></div>
+	<div id="ScheduleTab" class="Block"></div>
+	<!-- 四天行程 -->
+    <div id="schedule1" class="ArticleBox"></div>
+    <div id="price1" class="ArticleBox"></div>
+	<!-- 五天行程 -->
+	<div id="schedule2" class="ArticleBox"></div>
+    <div id="price2" class="ArticleBox"></div>
+
+	<!-- 共用 -->
     <div id="price" class="ArticleBox"></div>
     <div id="notice" class="ArticleBox"></div>
     <div id="agency" class="ArticleBox"></div>
