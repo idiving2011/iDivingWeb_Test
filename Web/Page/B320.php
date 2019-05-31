b128ddf0b4fcbf1278df1bc1f9a14a244c75ae9c
LukeHong <luke0724@hotmail.com>
2019-06-01 03:56:19 +0800

    移除大量重複的程式碼

--- Web/Page/B320.php
+++ Web/Page/B320.php
@@ -1,29 +1,14 @@
-﻿<?php include "../Common.php" ?>
-<!DOCTYPE html>
-
-<html>
-<head>
-  <title></title>
-  <meta name="Extra" content="B320.php.less" />
-  <meta name="BasePath" content="../" />
-  <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
-	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
-  <script type="text/javascript" src="../Script/jquery.js"></script>
-  <script type="text/javascript" src="../Script/Loader.js"></script>
-</head>
-<body>
-  <?php include "../Part/Head.php"; ?>
-  <?php include "../Part/Cap.php"; ?>
-  <div id="section" sight>
-		<div id="intro" class="ArticleBox"></div>
-		<div id="BC" class="ArticleBox"></div>
-		<div id="Regulators" class="ArticleBox"></div>
-		<div id="SS" class="ArticleBox"></div>
-		<div id="Instruments" class="ArticleBox"></div>
-		<div id="Computer" class="ArticleBox"></div>
-		<div id="Sets" class="ArticleBox"></div>
-    <div id="camera" class="ArticleBox"></div>
-  </div>
-  <?php include "../Part/Foot.php" ; ?>
+﻿<?php include "../Part/Header.php" ?>
+    <div id="section" sight>
+            <div id="intro" class="ArticleBox"></div>
+            <div id="BC" class="ArticleBox"></div>
+            <div id="Regulators" class="ArticleBox"></div>
+            <div id="SS" class="ArticleBox"></div>
+            <div id="Instruments" class="ArticleBox"></div>
+            <div id="Computer" class="ArticleBox"></div>
+            <div id="Sets" class="ArticleBox"></div>
+        <div id="camera" class="ArticleBox"></div>
+    </div>
+    <?php include "../Part/Foot.php" ; ?>
 </body>
 </html>
---
+++ Web/Part/Footer.php
@@ -0,0 +1,16 @@
+    <div id="footer" sight>
+        <span class="CopyRight">營業時間：周一公休，周二-六：4-10月 14:00 - 22:00 / 11-3月 15:00-21:00，周日：13:00 - 19:00</span><br/>
+        <span class="CopyRight">愛潛水股份有限公司版權所有</span>
+        <span class="CopyRight">© 2013 iDiving Co., Ltd.</span><br style="display:block" />
+        <span class="Address">電話：<a href="tel:0228827696">0228827696</a></span>
+        <span class="Address">地址：<a href="https://maps.google.com.tw/maps?q=台北市士林區前港街8號" target="_blank">台北市士林區前港街8號</a></span><br/>
+        <span class="Address">傳真：<a href="fax:0228827656">0228827656</a></span>
+        <span class="Address">E-mail：<a href="mailto:info@idiving.com.tw">info@idiving.com.tw</a></span>
+        <div class="Guide" hidden>
+            <a url="../Guide/Browser/Compatibility.html" class="Tip">瀏覽器相容性</a>
+            <span>|</span>
+            <a url="../Guide/PrivacyPolicy/Intro.html" class="Tip">隱私權政策</a>
+        </div>
+    </div>
+</body>
+</html>
