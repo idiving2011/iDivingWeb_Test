2b77459bbbf2cbe3bd5eda6737c6f2d8d19faaa8
YUAN <YUAN@DESKTOP-YUAN>
2019-08-02 19:35:32 +0800

    新增獵人三號船宿內容，並tab方式呈現。

--- Web/Page/D410.php
+++ Web/Page/D410.php
@@ -18,8 +18,13 @@
     <div id="intro" class="ArticleBox"></div>
     <div id="DiveSite" class="ArticleBox"></div>
     <div id="schedule" class="ArticleBox"></div>
-    <div id="price" class="ArticleBox"></div>
-    <div id="notice" class="ArticleBox"></div>
+	<div id="ScheduleTab" class="Block"></div>
+	<div id="Schedule1" class="ArticleBox"></div>
+	<div id="Schedule2" class="ArticleBox"></div>
+    <div id="price1" class="ArticleBox"></div>
+	<div id="price2" class="ArticleBox"></div>
+    <div id="notice1" class="ArticleBox"></div>
+	<div id="notice2" class="ArticleBox"></div>
     <div id="agency" class="ArticleBox"></div>
   </div>
   <?php include "../Part/Foot.php" ; ?>
