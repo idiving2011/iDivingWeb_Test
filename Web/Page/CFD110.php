56f42e743459bb3e9cd7c8942fecd1c18923fdb0
YUAN <YUAN@DESKTOP-YUAN>
2019-09-16 21:34:13 +0800

    各級自潛員改以tab方式呈現。

--- Web/Page/CFD110.php
+++ Web/Page/CFD110.php
@@ -17,22 +17,43 @@
     <div id="section" sight>
 		<div id="intro" class="ArticleBox"></div>
 		<div id="FreeDivingTab" class="Block"></div>
-		<div id="desc1" class="ArticleBox"></div>
-		<div id="curriculum1" class="ArticleBox"></div>
-		<div id="desc2" class="ArticleBox"></div>
-		<div id="curriculum2" class="ArticleBox"></div>
-		<div id="desc3" class="ArticleBox"></div>
-		<div id="curriculum3" class="ArticleBox"></div>
-		<div id="desc4" class="ArticleBox"></div>
-		<div id="curriculum4" class="ArticleBox"></div>
-		<div id="desc5" class="ArticleBox"></div>
-		<div id="curriculum5" class="ArticleBox"></div>
-		<div id="curriculum" class="ArticleBox"></div>
+
+		<div id="LV1desc" class="ArticleBox"></div>
+		<div id="LV1curriculum1" class="ArticleBox"></div>
+		<div id="LV1curriculum2" class="ArticleBox"></div>
+		<div id="LV1curriculum3" class="ArticleBox"></div>
+		<div id="LV2desc" class="ArticleBox"></div>
+		<div id="LV2curriculum" class="ArticleBox"></div>
+		<div id="LV3desc" class="ArticleBox"></div>
+		<div id="LV3curriculum" class="ArticleBox"></div>
+		<div id="BASICdesc" class="ArticleBox"></div>
+		<div id="BASICcurriculum1" class="ArticleBox"></div>
+		<div id="BASICcurriculum2" class="ArticleBox"></div>
+		<div id="POOLdesc" class="ArticleBox"></div>
+		<div id="POOLcurriculum1" class="ArticleBox"></div>
+		<div id="POOLcurriculum2" class="ArticleBox"></div>
+
+		
+		<div id="LV1plan1" class="ArticleBox"></div>
+		<div id="LV1plan2" class="ArticleBox"></div>
+		<div id="LV1plan3" class="ArticleBox"></div>
+		<div id="LV2plan" class="ArticleBox"></div>
+		<div id="LV3plan" class="ArticleBox"></div>
 		<div id="plan" class="ArticleBox"></div>
-		<div id="tuition" class="ArticleBox"></div>
+
+
+		<div id="LV1tuition" class="ArticleBox"></div>
+		<div id="LV2tuition" class="ArticleBox"></div>
+		<div id="LV3tuition" class="ArticleBox"></div>
+		<div id="BASICtuition" class="ArticleBox"></div>
+		<div id="POOLtuition" class="ArticleBox"></div>
+
+
+
 		<div id="register" class="ArticleBox"></div>
 		<div id="processing" class="ArticleBox"></div>
 		<div id="notice" class="ArticleBox"></div>
+
     </div>
     <?php include "../Part/Foot.php" ; ?>
 </body>
