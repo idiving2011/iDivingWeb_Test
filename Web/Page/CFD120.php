abe7cfe9c6eec54eb2f0f41f020b370f5593a167
YUAN <YUAN@DESKTOP-YUAN>
2019-10-16 10:53:44 +0800

    新增 FD 專長課程。

---
+++ Web/Page/CFD120.php
@@ -0,0 +1,93 @@
+﻿<?php include "../Common.php" ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+    <meta name="Extra" content="" />
+    <meta name="BasePath" content="../" />
+    <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
+	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
+    <script type="text/javascript" src="../Script/jquery.js"></script>
+    <script type="text/javascript" src="../Script/Loader.js"></script>
+</head>
+<body>
+    <?php include "../Part/Head.php"; ?>
+    <?php include "../Part/Cap.php"; ?>
+    <div id="section" sight>
+		<div id="intro" class="ArticleBox"></div>
+		<div id="FreeDivingTab" class="Block"></div>
+
+		<div id="1-1desc" class="ArticleBox"></div>
+		<div id="1-1curriculum" class="ArticleBox"></div>
+
+		<div id="1-2curriculum" class="ArticleBox"></div>
+
+		<div id="1-3curriculum" class="ArticleBox"></div>
+
+		<div id="1-4curriculum" class="ArticleBox"></div>
+
+		<div id="2-1desc" class="ArticleBox"></div>
+		<div id="2-1curriculum" class="ArticleBox"></div>
+
+		<div id="2-2desc" class="ArticleBox"></div>
+		<div id="2-2curriculum" class="ArticleBox"></div>
+
+		<div id="2-3desc" class="ArticleBox"></div>
+		<div id="2-3curriculum" class="ArticleBox"></div>
+
+		<div id="2-4desc" class="ArticleBox"></div>
+		<div id="2-4curriculum" class="ArticleBox"></div>
+
+		<div id="3-1desc" class="ArticleBox"></div>
+		<div id="3-1curriculum" class="ArticleBox"></div>
+
+		<div id="3-2desc" class="ArticleBox"></div>
+		<div id="3-2curriculum" class="ArticleBox"></div>
+
+		<div id="3-3desc" class="ArticleBox"></div>
+		<div id="3-3curriculum" class="ArticleBox"></div>
+
+		
+		
+		<div id="LV2desc" class="ArticleBox"></div>
+		<div id="LV2curriculum" class="ArticleBox"></div>
+		<div id="LV3desc" class="ArticleBox"></div>
+		<div id="LV3curriculum" class="ArticleBox"></div>
+		<div id="BASICdesc" class="ArticleBox"></div>
+		<div id="BASICcurriculum1" class="ArticleBox"></div>
+		<div id="BASICcurriculum2" class="ArticleBox"></div>
+		<div id="POOLdesc" class="ArticleBox"></div>
+		<div id="POOLcurriculum1" class="ArticleBox"></div>
+		<div id="POOLcurriculum2" class="ArticleBox"></div>
+
+		
+		<div id="LV1plan1" class="ArticleBox"></div>
+		<div id="LV1plan2" class="ArticleBox"></div>
+		<div id="LV1plan3" class="ArticleBox"></div>
+		<div id="LV2plan" class="ArticleBox"></div>
+		<div id="LV3plan" class="ArticleBox"></div>
+		<div id="plan" class="ArticleBox"></div>
+
+
+		<div id="1-1tuition" class="ArticleBox"></div>
+		<div id="1-2tuition" class="ArticleBox"></div>
+		<div id="1-4tuition" class="ArticleBox"></div>
+		<div id="2-1tuition" class="ArticleBox"></div>
+		<div id="2-2tuition" class="ArticleBox"></div>
+		<div id="2-3tuition" class="ArticleBox"></div>
+		<div id="2-4tuition" class="ArticleBox"></div>
+		<div id="3-1tuition" class="ArticleBox"></div>
+		<div id="3-2tuition" class="ArticleBox"></div>
+		<div id="3-3tuition" class="ArticleBox"></div>
+
+
+
+		<div id="register" class="ArticleBox"></div>
+		<div id="processing" class="ArticleBox"></div>
+		<div id="notice" class="ArticleBox"></div>
+
+    </div>
+    <?php include "../Part/Foot.php" ; ?>
+</body>
+</html>
