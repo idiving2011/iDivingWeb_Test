e1082b491916e09e117643f233b7e45b39e4dd5c
YUAN <YUAN@DESKTOP-YUAN>
2019-11-21 22:32:23 +0800

    新增延伸領域高氧潛水員

---
+++ Web/Page/C510.php
@@ -0,0 +1,27 @@
+﻿<?php include "../Common.php" ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title></title>
+  <meta name="Extra" content="" />
+  <meta name="BasePath" content="../" />
+  <meta name="Data" content = "<?php echo getDataFileName(""); ?>"/>
+	<link rel="shortcut icon" href="/Image/Icon/favicon/favicon.ico">
+  <script type="text/javascript" src="../Script/jquery.js"></script>
+  <script type="text/javascript" src="../Script/Loader.js"></script>
+</head>
+<body>
+  <?php include "../Part/Head.php"; ?>
+  <?php include "../Part/Cap.php"; ?>
+  <div id="section" sight>
+	<div id="area1" class="ArticleBox"></div>
+	<div id="area2" class="ArticleBox"></div>
+	<div id="area3" class="ArticleBox"></div>
+	<div id="area4" class="ArticleBox"></div>
+	<div id="area5" class="ArticleBox"></div>
+	<div id="area6" class="ArticleBox"></div>
+  </div>
+  <?php include "../Part/Foot.php" ; ?>
+</body>
+</html>
