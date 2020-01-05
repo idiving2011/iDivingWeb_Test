45479d182821532e08382cce95387f1f438e6f05
YUAN <YUAN@DESKTOP-YUAN>
2020-01-05 21:43:30 +0800

    建議行程：2天2夜與3天3夜

--- Web/Page/D120.php
+++ Web/Page/D120.php
@@ -17,7 +17,17 @@
   <div id="section" sight="">
     <div id="intro" class="ArticleBox"></div>
     <div id="DiveSite" class="ArticleBox"></div>
+
     <div id="schedule" class="ArticleBox"></div>
+    <div id="ScheduleTab" class="Block"></div>
+	<!-- 二天行程 -->
+    <div id="schedule1" class="ArticleBox"></div>
+    <div id="price1" class="ArticleBox"></div>
+	<!-- 三天行程 -->
+	<div id="schedule2" class="ArticleBox"></div>
+    <div id="price2" class="ArticleBox"></div>
+
+    <!-- 共用 -->
     <div id="price" class="ArticleBox"></div>
     <div id="notice" class="ArticleBox"></div>
     <div id="agency" class="ArticleBox"></div>
